a61c5abf6f3179ea930e01f13c8b39ec6db9dbab
Jessica <calajess@gmail.com>
2024-09-24 08:01:34 -0600

    fix: production environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
-  production: true,
+  production: false,
   baseUrl: '',
   useHash: false,
-  pageSize: 6,
+  pageSize: 5,
   apiPath: 'https://api-cursoangular.cursos-dev.com',
+  apiPathRandomUser: 'https://randomuser.me/api/',
+  recaptchaKey: '6LcqVe8cAAAAAFKrdDeUow8MCAXl4XGMsm7XbS-T',
 };
